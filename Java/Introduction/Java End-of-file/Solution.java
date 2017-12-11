0098a0d8da5bdeeae07080e36be7ded5e9448317
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2017-12-12 01:02:20 +0530

    Create Solution.java

---
+++ Java/Introduction/Java End-of-file/Solution.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Bilakhiya
+ */
+
+public class Solution {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        
+        
+        int i=1;
+        Scanner sc1=new Scanner(System.in);
+        String s;
+        while(sc1.hasNext())
+        {
+         s=sc1.nextLine();
+         
+         System.out.println(i+" "+s);i++;
+        }
+        
+       
+        
+    }
+        
+        
+    }
+    
